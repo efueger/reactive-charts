585c12d4ac02853812f7139380c99389b503e675
efueger <efueger@users.noreply.github.com>
2017-02-23 14:38:12 -0500

    Update io.js

--- src/io.js
+++ src/io.js
@@ -1,22 +1,15 @@
-/* @flow */
-
-/* IO monad based on that from monet.js. Modified to be more inlined with Folktale datatypes and
- * classed to be easier to use with flow type system */
+/* IO monad taken from monet.js */
 /*
 The MIT License (MIT)
-
 Copyright (c) 2016 Chris Myers
-
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 copies of the Software, and to permit persons to whom the Software is
 furnished to do so, subject to the following conditions:
-
 The above copyright notice and this permission notice shall be included in all
 copies or substantial portions of the Software.
-
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -24,49 +17,44 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
-
 */
 /* eslint-disable fp/no-this, fp/no-nil, fp/no-mutation, fp/no-throw */
 import * as R from 'ramda';
 
 
-// Here we're against fp rules but we're preparing Monad which we would eventually
-// had to do sugar-less style with violating fp/no-this, fp/no-nil, fp/no-mutation.
-// And with class we get better flow-type support
-/* eslint-disable fp/no-class, fp/no-this, fp/no-nil, fp/no-mutation, fp/no-throw */
-export class IO<T> {
-  effectFn: () => T;
+export const IO = (effectFn) => {
+  return new IO.fn.init(effectFn)
+};
 
-  chain: any;
-  perform: any;
-  performUnsafeIO: any;
+IO.of = (a) => {
+  return IO(() => {
+    return a;
+  })
+};
 
-  constructor(effectFn: () => T) {
+IO.fn = IO.prototype = {
+  init: (effectFn) => {
     if (!R.is(Function, effectFn))
-      throw 'IO Requires a function';
+      throw 'IO requires a function';
     this.effectFn = effectFn;
-  }
-
-  map(fn: any) {
-    return new IO(() => {
+  },
+  map: (fn) => {
+    return IO(function () {
       return fn(this.effectFn());
-    });
-  }
+    })
+  },
+  bind: (fn) => {
+    return IO(() => {
+      return fn(this.effectFn())
+        .run();
+    })
+  },
+  ap: (ioWithFn) => {
+    return ioWithFn.map((fn) => fn(this.effectFn()));
+  },
+  run: () => this.effectFn()
+};
 
-  bind(fn: any) {
-    return new IO(() => {
-      return fn(this.effectFn()).run();
-    });
-  }
-
-  ap(ioWithFn: any) {
-    return ioWithFn.map(fn => fn(this.effectFn()));
-  }
-
-  run() {
-    return this.effectFn();
-  }
-}
+IO.fn.init.prototype = IO.fn;
 
 IO.prototype.perform = IO.prototype.performUnsafeIO = IO.prototype.run;
-IO.prototype.chain = IO.prototype.bind;
